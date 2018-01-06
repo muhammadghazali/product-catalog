771ee84cf58eb498411cfd9a3f899c361c099b51
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2018-01-06 17:29:01 +0700

    initial product list mockup

--- src/App.js
+++ src/App.js
@@ -4,11 +4,36 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div class="grid-container">
-        <div class="grid-x grid-margin-x">
-          <div class="cell small-4">TBD</div>
-          <div class="cell small-4">TBD</div>
-          <div class="cell small-4">TBD</div>
+      <div className="grid-container">
+        <div className="grid-x grid-margin-x">
+          <div className="cell small-12">
+            <div className="product-list">
+              <div className="card card-product">
+                <div className="card-product-img-wrapper">
+                  <a href="#">
+                    <img src="https://placeimg.com/640/480/people" />
+                  </a>
+                </div>
+
+                <div className="card-section">
+                  <a href="#">
+                    <h3 className="card-product-name">
+                      Kickin with Kraken Tee
+                    </h3>
+                  </a>
+                  <h5 className="card-product-price">$19.99</h5>
+                  <p className="card-product-description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Proin posuere sem enim, accumsan convallis risus semper.
+                  </p>
+
+                  <button type="button" className="success button">
+                    Quick Look
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );

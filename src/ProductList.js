ec9eeecb99e906c3e0dc1ed189aa84f228eb5841
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2018-01-06 23:58:00 +0700

    use react-infinite-scroller to implement the infinite scroller
    issue #1

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,30 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
+import InfiniteScroll from 'react-infinite-scroller';
+import Chance from 'chance';
 
 import ProductListService from './services/ProductListService';
 
-const renderProductList = data => {
-  return data.map(productData => {
+const chance = new Chance();
+
+class ProductList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [],
+      hasMoreItems: true
+    };
+
+    this.loadMoreItems = this.loadMoreItems.bind(this);
+  }
+
+  renderProductList(data) {
+    return data.map(productData => {
+      return (
+        <div className="card card-product" key={chance.guid()}>
+          <div className="card-product-img-wrapper">
+            <img src={productData.image} alt={productData.name} />
+          </div>
+
+          <div className="card-section">
+            <h3 className="card-product-name">{productData.name}</h3>
+            <h5 className="card-product-price">Rp {productData.price}</h5>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  loadMoreItems() {
+    this.setState({
+      products: this.state.products.concat(ProductListService.getList())
+    });
+    console.log('more items');
+  }
+
+  loaderElement() {
+    return <div className="loader">Loading ...</div>;
+  }
+
+  render() {
     return (
-      <div className="card card-product">
-        <div className="card-product-img-wrapper">
-          <img src={productData.image} alt={productData.name} />
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={this.loadMoreItems}
+        hasMore={this.state.hasMoreItems}
+        loader={this.loaderElement()}
+        initialLoad={true}
+      >
+        <div className="product-list">
+          {this.renderProductList(this.state.products)}
         </div>
-
-        <div className="card-section">
-          <h3 className="card-product-name">{productData.name}</h3>
-          <h5 className="card-product-price">Rp {productData.price}</h5>
-        </div>
-      </div>
+      </InfiniteScroll>
     );
-  });
-};
-
-const ProductList = props => {
-  return (
-    <div className="product-list">
-      {renderProductList(ProductListService.getList())}
-    </div>
-  );
-};
-
+  }
+}
 export default ProductList;

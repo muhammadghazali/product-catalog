c9ec3f2f37d9a413fba5d5d059c36113a2469c0f
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2018-01-11 23:07:57 +0700

    update the `create product` form
    issue #4

--- src/CreateProductForm.js
+++ src/CreateProductForm.js
@@ -1,73 +1,124 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { CirclePicker } from 'react-color';
+class CreateProductForm extends Component {
+  constructor(props) {
+    super(props);
 
-const CreateProductForm = props => {
-  return (
-    <div>
-      <form>
-        <div className="grid-container">
-          <div className="grid-x grid-padding-x">
-            <div className="medium-6 cell">
-              <h1>Add Product</h1>
-              <div className="grid-x grid-padding-x">
-                <div className="medium-12 cell">
-                  <label>
-                    Name
-                    <input type="text" placeholder="Product name" />
-                  </label>
-                </div>
+    this.setState = {};
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-                <div className="medium-12 cell">
-                  <label>
-                    Description
-                    <textarea placeholder="Description of the product" />
-                  </label>
-                </div>
+  handleChange(e) {
+    console.log('handleChange', e);
+  }
 
-                <div className="medium-12 cell">
-                  <label>
-                    Color
-                    <CirclePicker />
-                    <br />
-                  </label>
-                </div>
+  render() {
+    return (
+      <div>
+        <form>
+          <div className="grid-container">
+            <div className="grid-x grid-padding-x">
+              <div className="medium-6 cell">
+                <h1>Add Product</h1>
+                <div className="grid-x grid-padding-x">
+                  <div className="medium-12 cell">
+                    <label>
+                      Name
+                      <input
+                        type="text"
+                        placeholder="Product name"
+                        name="product-name"
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                  </div>
 
-                <div className="medium-12 cell">
-                  <label>
-                    Price
-                    <input type="number" placeholder="Price of the product" />
-                  </label>
-                </div>
+                  <div className="medium-12 cell">
+                    <label>
+                      Description
+                      <textarea
+                        placeholder="Description of the product"
+                        name="product-desc"
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                  </div>
 
-                <div className="medium-12 cell">
-                  <fieldset className="fieldset">
-                    <legend>Size</legend>
-                    <input id="checkbox12" type="checkbox" />
-                    <label htmlFor="checkbox12">Checkbox 1</label>
-                    <input id="checkbox22" type="checkbox" />
-                    <label htmlFor="checkbox22">Checkbox 2</label>
-                    <input id="checkbox32" type="checkbox" />
-                    <label htmlFor="checkbox32">Checkbox 3</label>
-                  </fieldset>
-                </div>
+                  <div className="medium-12 cell">
+                    <label>
+                      Color
+                      <CirclePicker />
+                      <br />
+                    </label>
+                  </div>
 
-                <div className="medium-12 cell">
-                  <input
-                    type="submit"
-                    className="button"
-                    value="Save"
-                    title="Save to catalog"
-                  />
+                  <div className="medium-12 cell">
+                    <label>
+                      Price
+                      <input
+                        type="number"
+                        placeholder="Price of the product"
+                        name="product-price"
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                  </div>
+
+                  <div className="medium-12 cell">
+                    <fieldset className="fieldset">
+                      <legend>Size</legend>
+                      <input
+                        id="S"
+                        type="checkbox"
+                        name="product-size"
+                        onChange={this.handleChange}
+                      />
+                      <label htmlFor="S">S</label>
+
+                      <input
+                        id="M"
+                        type="checkbox"
+                        name="product-size"
+                        onChange={this.handleChange}
+                      />
+                      <label htmlFor="M">M</label>
+
+                      <input
+                        id="M"
+                        type="checkbox"
+                        name="product-size"
+                        onChange={this.handleChange}
+                      />
+                      <label htmlFor="L">L</label>
+
+                      <input
+                        id="XL"
+                        type="checkbox"
+                        name="product-size"
+                        onChange={this.handleChange}
+                      />
+                      <label htmlFor="XL">XL</label>
+                    </fieldset>
+                  </div>
+
+                  <div className="medium-12 cell">
+                    <input
+                      type="submit"
+                      className="button"
+                      value="Save"
+                      title="Save to catalog"
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div className="medium-6 cell">x</div>
+              <div className="medium-6 cell">x</div>
+            </div>
           </div>
-        </div>
-      </form>
-    </div>
-  );
-};
+        </form>
+      </div>
+    );
+  }
+}
 
 export default CreateProductForm;

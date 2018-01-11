6a672c4176fb9d049005b33eed70c103106b1110
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2018-01-11 21:51:31 +0700

    add more details to product page
    issue #2

--- src/ProductDetailsPage.js
+++ src/ProductDetailsPage.js
@@ -7,6 +7,12 @@ const ProductDetailsPage = ({ match }) => {
     product => product.id === match.params.productId
   );
 
+  const renderProductSize = sizeList => {
+    const list = sizeList.map(sizeItem => <li>{sizeItem} </li>);
+
+    return <ul>{list}</ul>;
+  };
+
   return (
     <div className="card card-product">
       <div className="card-product-img-wrapper">
@@ -15,7 +21,15 @@ const ProductDetailsPage = ({ match }) => {
 
       <div className="card-section">
         <h3 className="card-product-name">{matchedProducts[0].name}</h3>
-        <h5 className="card-product-price">Rp {matchedProducts[0].price}</h5>
+        <p className="card-product-price">Rp {matchedProducts[0].price}</p>
+        <h5>Size</h5>
+        <p>{renderProductSize(matchedProducts[0].size)}</p>
+        <h5>Description</h5>
+        <p className="card-product-price">
+          Rp {matchedProducts[0].description}
+        </p>
+        <h5>Materials</h5>
+        <p className="card-product-materials">{matchedProducts[0].materials}</p>
       </div>
     </div>
   );
